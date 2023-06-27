40121c792ce1fe855fc7de93fe868986036da81d
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-27 20:42:03 +0200

    zbieranie danych

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,20 +1,50 @@
-import { useState } from 'react';
-import './UsersList.css';
+import { useState } from "react";
+import "./UsersList.css";
 
 const UsersList = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    usertype: ""
+  });
 
-    const [username, setUserName] = useState('')
-
-  return <div className="usersList">
-    <h2>{username}</h2>
-    <form>
+  const handleInputChange = (e) => {
+    const target = e.target;
+    const name = target.name
+    setFormData(prevDataForm=>{
+        return {...prevDataForm, [name]: target.value};
+    });
+  };
+console.log(formData);
+  return (
+    <div className="usersList">
+      <h2>{formData.username}</h2>
+      <form>
         <label htmlFor="username">User name</label>
-        <input type="text" id="username" name="username" placeholder="User name" onChange={(e)=>{setUserName(e.target.value)}}/>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="User name"
+          onChange={handleInputChange}
+        />
         <label htmlFor="email">User e-mail</label>
-        <input type="email" id="email" name="email" placeholder="User e-mail"/>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="User e-mail"
+          onChange={handleInputChange}
+        />
+        <label htmlFor="usertype">User name</label>
+        <select name="usertype" id="usertype" onChange={handleInputChange}>
+          <option value="Admin">Admin</option>
+          <option value="User">User</option>
+        </select>
         <button>Save</button>
-    </form>
-  </div>;
+      </form>
+    </div>
+  );
 };
 
-export default UsersList
+export default UsersList;
