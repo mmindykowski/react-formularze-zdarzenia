28184b6e6d20d0be9547d767a534d0dab16ca3b8
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-27 20:17:32 +0200

    stylowanie formularza

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+import './UsersList.css';
+
 const UsersList = () => {
-  return <div className="usersList">To będzie lista userów</div>;
+
+    const [username, setUserName] = useState()
+
+  return <div className="usersList">
+    <form>
+        <label htmlFor="username">User name</label>
+        <input type="text" id="username" name="username" placeholder="User name"/>
+        <label htmlFor="email">User e-mail</label>
+        <input type="email" id="email" name="email" placeholder="User e-mail"/>
+        <button>Save</button>
+    </form>
+  </div>;
 };
 
-export default UsersList;
+export default UsersList
