0bde44ba5876156417b678ba15a38775dc6d90a5
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-27 20:26:54 +0200

    obsluga zdarzenia

--- src/UsersList.js
+++ src/UsersList.js
@@ -3,12 +3,13 @@ import './UsersList.css';
 
 const UsersList = () => {
 
-    const [username, setUserName] = useState()
+    const [username, setUserName] = useState('')
 
   return <div className="usersList">
+    <h2>{username}</h2>
     <form>
         <label htmlFor="username">User name</label>
-        <input type="text" id="username" name="username" placeholder="User name"/>
+        <input type="text" id="username" name="username" placeholder="User name" onChange={(e)=>{setUserName(e.target.value)}}/>
         <label htmlFor="email">User e-mail</label>
         <input type="email" id="email" name="email" placeholder="User e-mail"/>
         <button>Save</button>
